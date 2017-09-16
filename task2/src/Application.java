9bdeed7ad712c460f5372cc6c66b1fb638988bd7
Polina <polina.serikova.91@gmail.com>
2017-09-17 02:13:50 +0300

    #1 - refactoring, #2 - random from double to int, #3 - comments for methods added

--- task2/src/Application.java
+++ task2/src/Application.java
@@ -1,18 +1,24 @@
 import java.util.OptionalDouble;
 import java.util.Random;
 import java.util.stream.IntStream;
+import java.math.RoundingMode;
 
 public class Application {
     public static void main(String[] args) {
         Student[] groupA = new Student[5];
+        CreateStudents(groupA);
+        int[] results = PassExam(groupA);
+        PrintAverageResult(results);
+    }
+/* Create list of student using 'Student' ctor */
+    private static void CreateStudents(Student[] groupA) {
         groupA[0] = new Student("Ivan","Ivanov");
         groupA[1] = new Student("Boris","Borisov");
         groupA[2] = new Student("Kolya","Kolynov");
         groupA[3] = new Student("Petr","Petrov");
         groupA[4] = new Student("Semen","Semenov");
-        int[] results = PassExam(groupA);
-        PrintAverageResult(results);
     }
+/* Create array 'results' and define random value */
     public static int[] PassExam(Student[] groupA)
     {
         int[] results = new int[groupA.length];
@@ -20,18 +26,21 @@ public class Application {
         {
             Random randomInt = new Random();
             results[i]=randomInt.nextInt(5)+1;
+            //It's not by task, but I prefer to print results before print average
             System.out.println(results[i]);
         }
         return results;
     }
+/* Calculate sum of results and print average value */
     public static void PrintAverageResult(int[] results)
     {
-        int sum=0;
+        double sum=0;
         for (int x:results)
         {
          sum+=x;
         }
-        double average =average=(sum/(results.length));
+        // Nothing said about type of average
+        int average =(int)Math.round(sum/results.length);
         System.out.println("Average result of group: "+average);
     }
 }
