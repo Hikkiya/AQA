d438234ca849ed54110c89f4560d0fbb8f647263
Polina <polina.serikova.91@gmail.com>
2017-09-16 22:56:13 +0300

    First Commit

---
+++ task1/src/HelloWorld.java
@@ -0,0 +1,25 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        TextWriter.PrintText("Hello, world.");
+    }
+}
+/* Class for working with text */
+class TextWriter {
+    /* Print sting to console */
+    public static void PrintText(String text) {
+        System.out.println(text);
+    }
+    /* Calculate amount of symbols in string */
+    public int GetSymbolsAmount(String text) {
+        //Coming soon...
+        int count=0;
+        return count;
+    }
+    /* Upper case to lower case */
+    public String TextToLower (String text) {
+        //Coming soon...
+        String toLower= null;
+        return toLower;
+    }
+}
