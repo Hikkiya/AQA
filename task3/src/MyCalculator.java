ea74677ee15c773be48c474ba4f81ddf767d2aba
Polina <polina.serikova.91@gmail.com>
2017-09-18 23:05:07 +0300

    calc beta version. Exceptions processed, tests pass.

--- task3/src/MyCalculator.java
+++ task3/src/MyCalculator.java
@@ -7,22 +7,21 @@ public class MyCalculator implements PrimitiveCalculator {
 
     public MyCalculator(double a, String o, double b) {
         if (o.equals("+")) {
-            System.out.println(a + " + " + b + " = " + add(a,b));
+            System.out.println("result = " + add(a,b));
             return;
         }
         if (o.equals("-")) {
-            System.out.println(a + " + " + b + " = " + sub(a,b));
+            System.out.println("result = " + sub(a,b));
             return;
         }
         if (o.equals("*")) {
-            System.out.println(a + " + " + b + " = " + mul(a,b));
+            System.out.println("result = " + mul(a,b));
             return;
         }
         if (o.equals("/")) {
-            System.out.println(a + " + " + b + " = " + div(a,b));
+            System.out.println("result = " + div(a,b));
             return;
         }
-        System.out.println("Unknown operation");
     }
 // overwrite interface method
     public double add(double a, double b) {
@@ -44,29 +43,75 @@ public class MyCalculator implements PrimitiveCalculator {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Please, enter parameters like this: 1 + 1, or write \"quit\" for exit");
+        System.out.println("Please, enter parameters like this: 1+1 and press ENTER. Write \"quit\" for exit");
         String enterText = in.nextLine();
         enterText.toLowerCase();
-        //while (enterText!="quit")
-            String[] parseText = Parser(enterText);
-            if (parseText!=null)
-            {
-                double first = Double.parseDouble(parseText[0]);
-                String operator = parseText[1];
-                double second = Double.parseDouble(parseText[2]);
-                new MyCalculator(first, operator, second);
-
-            }
-            else System.out.println("Enter parameters like this: 1 + 1");
+        if (!(enterText.equals("quit")))
+        {
+            CalculateDeliver(enterText); //call parser to execute values
+        }
     }
+
+    /* Delivering string to parser and calculating parsed values */
+    private static void CalculateDeliver(String enterText) {
+        String[] parseText = Parser(enterText);
+        if (parseText != null) {
+            try {
+                double first = Double.parseDouble(parseText[0]);
+                double second = Double.parseDouble(parseText[2]);
+            } catch (Exception ex) {
+                System.out.println("Please, enter the numbers. Use dot for separate double numbers.");
+                main(null);
+            }
+            double first = Double.parseDouble(parseText[0]);
+            double second = Double.parseDouble(parseText[2]);
+            String operator = parseText[1];
+            new MyCalculator(first, operator, second);
+            main(null);
+        } else {
+            main(null);
+        }
+    }
+
+    /* Parser. Cases: 1) more than 3 values 2) spaces 3) no operators */
     public static String[] Parser(String enterText)
     {
         String[] parsedArray = new String[3];
         String[] operator = enterText.split("[^+/*-]+");
         String[] values = enterText.split("[+*/-]");
-            parsedArray[0] = values[0];
-            parsedArray[1] = operator[1];
-            parsedArray[2] = values[1];
+        if(operator.length>2)
+        {
+            parsedArray=null;
             return parsedArray;
+        }
+        else if(operator.length==0)
+        {
+            System.out.println("Please, use operators like +,-,* and /");
+            parsedArray=null;
+            return parsedArray;
+        }
+        else
+        {
+            if(values.length>=3)
+            {
+                if(operator[0].equals("-"))
+                {
+                    parsedArray[0] = ((operator[0]+values[1])).replaceAll(" ", "");
+                    parsedArray[2] = values[2].replaceAll(" ", "");
+                }
+                else
+                {
+                    parsedArray=null;
+                    return parsedArray;
+                }
+            }
+            else
+            {
+                parsedArray[0] = (values[0]).replaceAll(" ", "");
+                parsedArray[2] = values[1].replaceAll(" ", "");
+            }
+            parsedArray[1] = operator[1];
+            return parsedArray;
+        }
     }
 }
